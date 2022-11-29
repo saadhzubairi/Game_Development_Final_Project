d1cd3f46101bf1ae9c7a0122919d603e821e9363
Saad Zubairi <saadhzubairi@outlook.com>
2022-11-29 12:23:04 +0500

    Added Terrain

---
+++ Utilities/StaticObjects.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Saad Zubairi on 11/29/2022.
+//
+
+#include "StaticObjects.h"
+
+SDL_Renderer* StaticObjects::renderer = nullptr;
---
+++ Utilities/StaticObjects.h
@@ -0,0 +1,17 @@
+//
+// Created by Saad Zubairi on 11/29/2022.
+//
+
+#ifndef GAME_STATICOBJECTS_H
+#define GAME_STATICOBJECTS_H
+
+#include "SDL.h"
+
+class StaticObjects {
+public:
+    static SDL_Renderer *renderer;
+
+};
+
+
+#endif //GAME_STATICOBJECTS_H
